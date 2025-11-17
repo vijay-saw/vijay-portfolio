692eab94f7b6ea399582c11b4bbf5693a31a09b9
root <root@anisble-config.3zu4a5dqkise1lnt5ksoleuyba.rx.internal.cloudapp.net>
2025-11-17 17:20:48 +0000

    added new file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8000/api";
+const API_BASE = `${import.meta.env.VITE_API_URL}/api`;
+export default API_BASE;
 
 export const getProfile = () => axios.get(`${API_BASE}/profile/`);
 export const getProjects = () => axios.get(`${API_BASE}/projects/`);
