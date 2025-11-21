a63aab9414fbd8b36f020726f557f046503796a8
root <root@anisble-config.3zu4a5dqkise1lnt5ksoleuyba.rx.internal.cloudapp.net>
2025-11-21 11:56:51 +0000

    added file changes

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from groq import Groq
+import os
 
 from .models import (
     Profile,
